e24ee4d438c8295b23a2709774901045a8ec287c
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-01-04 16:17:31 +0100

    Update MainAppBar.js

--- src/components/MainAppBar.js
+++ src/components/MainAppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as Logo } from '../assets/svgs/SolvLogoWhite.svg';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -26,7 +27,8 @@ export default function MainAppBar() {
       <AppBar position="static">
         <Toolbar>
 
-        <svg id="Vrstva_2" data-name="Vrstva 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 475.86 333"><defs><style>.cls-1{fill:#ff931e;}.cls-2{font-size:297.35px;fill:#fff;font-family:TrebuchetMS, Trebuchet MS;letter-spacing:-0.04em;}</style></defs><title>SolvLogoWhite</title><polygon class="cls-1" points="113.29 259 143.36 258.8 270.07 84.15 240.39 84.15 113.29 259"/><text class="cls-2" transform="translate(0 255.03)">solv</text><polygon class="cls-1" points="113.29 259 143.36 258.8 210.71 166.34 181.03 166.34 113.29 259"/></svg>
+        <Logo />
+
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
