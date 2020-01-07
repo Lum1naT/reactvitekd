3e1bdfb4ae8036abda28e281d7611eef25bb5a7f
K0jnCZ <k0jncz@gmail.com>
2020-01-07 09:20:48 +0100

    css

--- src/components/MainAppBar.js
+++ src/components/MainAppBar.js
@@ -28,7 +28,7 @@ export default function MainAppBar() {
       <AppBar position="static">
         <Toolbar>
 <SvgIcon>
-        <Logo />
+        <Logo className="app-logo" />
 </SvgIcon>
           <Button color="inherit">Login</Button>
         </Toolbar>
