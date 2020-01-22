fb2d8d35e4bbbaf7e77d1161602f5f25a4a79e57
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-01-22 20:37:29 +0100

    router

--- src/components/MainAppBar.js
+++ src/components/MainAppBar.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { ReactComponent as Logo } from '../assets/svgs/SolvLogoWhite.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home.js';
+import About from './About.js';
+import Contact from './Contact.js';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -26,12 +30,29 @@ export default function MainAppBar() {
   return (
     <div className={classes.root}>
       <AppBar position="static" className="headerBar">
-    
+
         <Toolbar>
 <SvgIcon className="headerLogo">
         <Logo />
 </SvgIcon>
-          <Button color="inherit">Login</Button>
+<Router>
+       <div>
+         <h2>Welcome to React Router Tutorial</h2>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+         <ul className="navbar-nav mr-auto">
+           <li><Link to={'/'} className="nav-link"> Home </Link></li>
+           <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+           <li><Link to={'/about'} className="nav-link">About</Link></li>
+         </ul>
+         </nav>
+         <hr />
+         <Switch>
+             <Route exact path='/' component={Home} />
+             <Route path='/contact' component={Contact} />
+             <Route path='/about' component={About} />
+         </Switch>
+       </div>
+     </Router>
         </Toolbar>
       </AppBar>
     </div>
