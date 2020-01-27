0e94adf3b1ab144c123659433996ac17686c672f
K0jnCZ <k0jncz@gmail.com>
2020-01-27 23:39:21 +0100

    ez

---
+++ dist/src/bitComponents/HelloWorld/HelloWorld.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = helloWorld;
+
+function helloWorld() {
+  return "Hello World. My bit works.";
+}
+
+//# sourceMappingURL=HelloWorld.js.map
