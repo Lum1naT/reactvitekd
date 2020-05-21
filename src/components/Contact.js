0fe44cb5b1752716b8d82e3df356f1615d926002
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-05-21 02:02:43 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,14 +13,6 @@ function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 },
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
 
 export default function CustomizedSnackbars() {
   const classes = useStyles();
